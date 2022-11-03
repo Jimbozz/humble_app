df98bd61a1e9245ff063b2fea20c351244af9c89
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-03 18:13:39 +0100

    updated heading component

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -4,6 +4,73 @@ function HomePage() {
   return (
     <>
       <Heading content="Home Page" />
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
+        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
+        consequatur quae, sequi adipisci.
+      </p>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
         reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
--- src/layout/Heading.jsx
+++ src/layout/Heading.jsx
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 function Heading({ size = "1", content }) {
   const VariableHeading = `h${size}`;
 
-  return <VariableHeading>{content}</VariableHeading>;
+  return (
+    <header className="heading-container">
+      <VariableHeading>{content}</VariableHeading>
+    </header>
+  );
 }
 
 Heading.propTypes = {
