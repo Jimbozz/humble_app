b7ec004b5c07dc0150364dba1b5cf19664b336ba
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-03 18:24:32 +0100

    added card placeholder

--- src/layout/ProfileCard.jsx
+++ src/layout/ProfileCard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function ProfileCard() {
   return (
     <aside className="profile-card-container">
-      <ul>
-        <li>Profile card</li>
-      </ul>
+      <Card bg="dark">
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Name of user</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </aside>
   );
 }
