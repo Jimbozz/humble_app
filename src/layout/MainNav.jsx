3d027a69da678211e82fad5ac8b1bd593979e4c5
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-03 18:14:24 +0100

    added content

--- src/layout/MainNav.jsx
+++ src/layout/MainNav.jsx
@@ -1,27 +1,31 @@
-import { Dropdown } from "react-bootstrap";
+import { Button, ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 
 function MainNav(props) {
   return (
     <aside className="sidebar">
-      <div>Brand name</div>
-      <div>
-        <ul>
+      <div className="sidebar-logo">
+        <img src={require("../assets/humble-logo.png")} alt="humble logo" />
+      </div>
+      <div className="sidebar-content">
+        <ul className="sidebar-content_list">
           <li>Home</li>
           <li>Profiles</li>
           <li>Profile</li>
+          <Button variant="primary" className="w-100">
+            Create post
+          </Button>
         </ul>
-        <Dropdown className="d-inline">
-          <Dropdown.Toggle id="dropdown-autoclose-true">
-            Default Dropdown
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-            <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-            <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        <hr></hr>
+        <ButtonGroup justified>
+          <DropdownButton
+            variant="dark"
+            id="dropdown-basic-button"
+            title="User name"
+            menuVariant="dark">
+            <Dropdown.Item href="#/action-1">Logout</Dropdown.Item>
+          </DropdownButton>
+        </ButtonGroup>
       </div>
     </aside>
   );
