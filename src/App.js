112267a41c7f48868299452f044a8536a5b22683
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-04 14:50:29 +0100

    added route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router, Switch } from "react-router-dom";
 import "./sass/styles.scss";
 import HomePage from "./components/home/HomePage";
 import Layout from "./layout/Layout";
 import ProfilesPage from "./components/profiles/ProfilesPage";
+import { AuthProvider } from "./context/AuthContext";
+import RegisterPage from "./components/register/RegisterPage";
 
 function App() {
   return (
-    <Layout>
+    <AuthProvider>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
         <Route exact path="/profiles" element={<ProfilesPage />} />
+
         {/* <Route exact path="/page/:id" element={<PageSpecific />} />
       <Route exact path="/login" element={<LoginPage />} />
       <Route path="/admin" element={<AdminPage />} />
       <Route path="/admin/edit/:id" element={<EditPage />} /> */}
       </Routes>
-    </Layout>
+    </AuthProvider>
   );
 }
 
