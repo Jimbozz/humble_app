5f93d7443956e18303372f5355241bc6a6358580
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-04 10:49:28 +0100

    added route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./sass/styles.scss";
 import HomePage from "./components/home/HomePage";
 import Layout from "./layout/Layout";
+import ProfilesPage from "./components/profiles/ProfilesPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/profiles" element={<ProfilesPage />} />
         {/* <Route exact path="/page/:id" element={<PageSpecific />} />
       <Route exact path="/login" element={<LoginPage />} />
       <Route path="/admin" element={<AdminPage />} />
