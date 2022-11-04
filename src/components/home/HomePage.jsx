99501915ee3bb9e5f8f642f5ee8d282d6256ff50
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-04 10:49:44 +0100

    added card placeholder

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,10 +1,20 @@
+import { Card } from "react-bootstrap";
 import Heading from "../../layout/Heading";
 
 function HomePage() {
   return (
     <>
       <Heading content="Home Page" />
-
+      <Card bg="dark">
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
         reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
