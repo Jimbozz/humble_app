6e98446606352aadf7a86c68223add9185916137
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-06 15:17:14 +0100

    added post list component

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,93 +1,12 @@
-import { Card } from "react-bootstrap";
 import Heading from "../../layout/Heading";
 import Layout from "../../layout/Layout";
+import PostsList from "../posts/PostsList";
 
 function HomePage() {
   return (
     <Layout>
       <Heading content="Home Page" />
-      <Card bg="dark">
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-        reiciendis placeat ab laborum reprehenderit. Possimus et eum sunt
-        repudiandae magni quae necessitatibus? Fuga eveniet sed commodi
-        consequatur quae, sequi adipisci.
-      </p>
+      <PostsList />
     </Layout>
   );
 }
