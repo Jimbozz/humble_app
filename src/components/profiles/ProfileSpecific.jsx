36504ca7372e21587950a63e79fe0b56198f7099
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-08 16:02:49 +0100

    Initial commit

---
+++ src/components/profiles/ProfileSpecific.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../../layout/Heading";
+import Layout from "../../layout/Layout";
+
+function ProfileSpecific() {
+  return (
+    <Layout>
+      <Heading content="Profile specific Page" />
+    </Layout>
+  );
+}
+
+export default ProfileSpecific;
