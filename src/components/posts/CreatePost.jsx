70801bf0d873b8ec821846f7f5c54c1696c69662
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-08 16:01:53 +0100

    initial commit

---
+++ src/components/posts/CreatePost.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../../layout/Heading";
+import Layout from "../../layout/Layout";
+
+function CreatePost() {
+  return (
+    <Layout>
+      <Heading content="Create post" />
+    </Layout>
+  );
+}
+
+export default CreatePost;
