90e2a2ee4e0d7482b9eb1e9780d8159556e287d1
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-08 16:02:10 +0100

    added modal

--- src/components/posts/OptionsButton.jsx
+++ src/components/posts/OptionsButton.jsx
@@ -1,27 +1,42 @@
-import React from "react";
-import { Button, Dropdown, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Dropdown, Modal, Navbar } from "react-bootstrap";
 import { BsThreeDots } from "react-icons/bs";
-import { NavLink } from "react-router-dom";
 
 function OptionsButton() {
-  return (
-    // <Button variant="options">
-    //   <BsThreeDots />
-    // </Button>
-    <Dropdown>
-      <Dropdown.Toggle variant="options" id="dropdown-basic">
-        <BsThreeDots />
-      </Dropdown.Toggle>
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-      <Dropdown.Menu variant="dark">
-        <NavLink to="/link_to_somewhere">
-          <div className="dropdown-item">Edit post</div>
-        </NavLink>
-        <NavLink to="/link_to_somewhere">
-          <div className="dropdown-item">Delete post</div>
-        </NavLink>
-      </Dropdown.Menu>
-    </Dropdown>
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="options" id="dropdown-basic">
+          <BsThreeDots />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item as="button" onClick={handleShow}>
+            Edit post
+          </Dropdown.Item>
+          <Dropdown.Item as="button">Delete post</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
