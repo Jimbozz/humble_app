fc0508acb5e7772b46bf5b89def275fece9148fc
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-08 16:03:07 +0100

    added route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Router, Switch } from "react-router-dom";
+import { Routes, Route, Router, Switch, useLocation } from "react-router-dom";
 import "./sass/styles.scss";
 import HomePage from "./components/home/HomePage";
 import Layout from "./layout/Layout";
@@ -7,20 +7,29 @@ import { AuthProvider } from "./context/AuthContext";
 import RegisterPage from "./components/register/RegisterPage";
 import LoginPage from "./components/login/LoginPage";
 
+import ProfileSpecific from "./components/profiles/ProfileSpecific";
+
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/profile/:id" element={<ProfileSpecific />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/profiles" element={<ProfilesPage />} />
-
-        {/* <Route exact path="/page/:id" element={<PageSpecific />} />
-      <Route exact path="/login" element={<LoginPage />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/admin/edit/:id" element={<EditPage />} /> */}
+        {/* <Route exact path="/create" element={<CreatePost />} /> */}
       </Routes>
+      {/* <Routes location={background || location}>
+        <Route path="/" element={<HomePage />}>
+          <Route path="modal" element={<OptionsButton />} />
+        </Route>
+      </Routes>
+      {background && (
+        <Routes>
+          <Route path="modal" element={<OptionsButton />} />
+        </Routes>
+      )} */}
     </AuthProvider>
   );
 }
