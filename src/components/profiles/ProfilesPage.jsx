9b2f6641be62aec6431fee7228e6c48ad1a71f3a
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-09 13:18:45 +0100

    added component

--- src/components/profiles/ProfilesPage.jsx
+++ src/components/profiles/ProfilesPage.jsx
@@ -1,21 +1,13 @@
 import { Card } from "react-bootstrap";
-import Heading from "../../layout/Heading";
+import Heading from "../common/Heading";
 import Layout from "../../layout/Layout";
+import ProfilesList from "./ProfilesList";
 
 function ProfilesPage() {
   return (
     <Layout>
       <Heading content="Profiles Page" />
-      <Card bg="dark">
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <ProfilesList />
     </Layout>
   );
 }
