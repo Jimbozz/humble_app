5d0566d8f7a38cfc6db8aecf8ea176138f0eb30a
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-15 15:19:14 +0100

    placeholder images + content

--- src/components/profile-specific/ProfileSpecific.jsx
+++ src/components/profile-specific/ProfileSpecific.jsx
@@ -5,13 +5,20 @@ import Layout from "../../layout/Layout";
 import useAxios from "../../hooks/useAxios";
 import Loading from "../common/Loading";
 import AlertError from "../common/AlertError";
-import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
+import { Col, Container, Image, Row, Tab, Tabs } from "react-bootstrap";
 import Posts from "./PostsList";
 import ProfileCard from "../profiles/ProfileCard";
 import { AlertInfo } from "../common/AlertInfo";
 import FollowersList from "./FollowersList";
+import placholderImage from "../../assets/profile-placeholder.png";
 
-function ProfileSpecific() {
+export default function ProfileSpecific() {
+  const placeholder = placholderImage;
+  const onImageError = (event) => {
+    event.target.onerror = null;
+    event.target.src = placeholder;
+  };
+
   let { id } = useParams();
   const url = "social/profiles/" + id + "?_following=true&_followers=true";
   const [profile, setProfile] = useState([]);
@@ -24,7 +31,7 @@ function ProfileSpecific() {
     async function getProfile() {
       try {
         const response = await http.get(url);
-        console.log(response.data);
+        console.log("response", response.data);
         setProfile(response.data);
       } catch (error) {
         setError(error.toString());
@@ -47,17 +54,44 @@ function ProfileSpecific() {
     );
   }
 
+  const hasFollowers = profile.followers.length;
+  const isFollowing = profile.following.length;
+
   return (
     <Container>
       <div className="user-info">
-        <div className="user-info__banner">Banner</div>
-        <div className="user-info__avatar">Avatar</div>
+        <div className="user-info__banner">
+          <Image
+            className="user-info__banner-image"
+            src={profile.banner ? profile.banner : placeholder}
+            alt={profile.name}
+            onError={onImageError}
+          />
+        </div>
+        <div className="user-info__avatar">
+          <Image
+            className="user-info__avatar-image"
+            src={profile.avatar ? profile.avatar : placeholder}
+            alt={profile.name}
+            onError={onImageError}
+          />
+        </div>
         <div className="user-info__email">{profile.email}</div>
       </div>
-      <Row className="justify-content-md-center mb-3">
-        <Col sm={2}>Followers {profile.followers.length}</Col>
-        <Col sm={2}>Following {profile.following.length}</Col>
-      </Row>
+      <div className="d-flex justify-content-center mb-3 user-follow">
+        <Col md="auto" className="d-flex align-items-center">
+          <span className="user-follow__number">
+            {profile.followers.length}
+          </span>{" "}
+          Followers
+        </Col>
+        <Col md="auto" className="d-flex align-items-center">
+          <span className="user-follow__number">
+            {profile.following.length}
+          </span>
+          Following
+        </Col>
+      </div>
       <Tabs
         defaultActiveKey="posts"
         id="justify-tab-example"
@@ -67,32 +101,43 @@ function ProfileSpecific() {
           <Posts />
         </Tab>
         <Tab eventKey="followers" title="Followers">
-          {/* {profile.followers.map((item) => {
-              const { name, avatar } = item;
-              return (
-                <Col key={name}>
-                  <ProfileCard name={name} avatar={avatar} />
-                </Col>
-              );
-            })} */}
-          <FollowersList />
+          <Row xs={1} className="g-4">
+            {hasFollowers ? (
+              profile.followers.map((item) => {
+                const { name, avatar } = item;
+                return (
+                  <Col key={name}>
+                    <ProfileCard name={name} avatar={avatar} />
+                  </Col>
+                );
+              })
+            ) : (
+              <Col>
+                <AlertInfo>This user has no followers</AlertInfo>
+              </Col>
+            )}
+          </Row>
+          {/* <FollowersList /> */}
         </Tab>
         <Tab eventKey="following" title="Following">
           <Row xs={1} className="g-4">
-            {/*Check to see if you can check for length and add an alert instead of calling for more data. */}
-            {/* {profile.following.map((item) => {
-              const { name, avatar } = item;
-              return (
-                <Col key={name}>
-                  <ProfileCard name={name} avatar={avatar} />
-                </Col>
-              );
-            })} */}
+            {isFollowing ? (
+              profile.following.map((item) => {
+                const { name, avatar } = item;
+                return (
+                  <Col key={name}>
+                    <ProfileCard name={name} avatar={avatar} />
+                  </Col>
+                );
+              })
+            ) : (
+              <Col>
+                <AlertInfo>This user is not following anyone.</AlertInfo>
+              </Col>
+            )}
           </Row>
         </Tab>
       </Tabs>
     </Container>
   );
 }
-
-export default ProfileSpecific;
