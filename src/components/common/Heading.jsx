121fe820aedebbfeb511bfe504142a0df76380ee
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-17 16:55:19 +0100

    added back button to header component

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,14 +1,28 @@
 import PropTypes from "prop-types";
-import { useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
 
 export default function Heading({ size = "1", content }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1, { replace: true });
+  };
+
   let { id } = useParams();
   const VariableHeading = `h${size}`;
 
   return (
     <header className="main-content__heading">
-      {id ? <div>Button</div> : null}
-      <VariableHeading>{content}</VariableHeading>
+      {id ? (
+        <Button variant="link" onClick={handleClick}>
+          <IoArrowBack />
+        </Button>
+      ) : null}
+      <VariableHeading className="main-content__heading-item">
+        {content}
+      </VariableHeading>
     </header>
   );
 }
