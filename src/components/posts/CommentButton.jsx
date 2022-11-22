35accdb5377e77bf8fe48d243b4aa8d1178c06c6
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-22 16:58:30 +0100

    initial

---
+++ src/components/posts/CommentButton.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { Alert, Button, Form, Modal } from "react-bootstrap";
+import { BiComment } from "react-icons/bi";
+import useAxios from "../../hooks/useAxios";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import FormError from "../common/FormError";
+import FormWarning from "../common/FormWarning";
+import PostCard from "./PostCard";
+import CommentModalPost from "./CommentModalPost";
+
+const schema = yup.object().shape({
+  body: yup
+    .string()
+    .required("You must add a comment.")
+    .min(1, "Comment should be at least 1 character"),
+});
+
+export default function CommentButton({
+  comments,
+  id,
+  title,
+  body,
+  author,
+  created,
+  media,
+}) {
+  const [submitting, setSubmitting] = useState(false);
+  const [createError, setCreateError] = useState(null);
+  const [updated, setUpdated] = useState(false);
+  const http = useAxios();
+  const url = "social/posts/" + id + "/comment";
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  async function onSubmit(data) {
+    setSubmitting(true);
+    setCreateError(null);
+    setUpdated(false);
+    console.log(data);
+
+    try {
+      const response = await http.post(url, data);
+      console.log(response.data);
+      setUpdated(true);
+    } catch (error) {
+      console.log(error);
+      setCreateError(error.toString());
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
+  return (
+    <>
+      <Button
+        variant="comment"
+        className="d-flex align-items-center"
+        onClick={handleShow}>
+        <BiComment className="btn-comment__icon" />
+        <small>{comments.length}</small>
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="modal-heading">Comment</Modal.Title>
+        </Modal.Header>
+        <CommentModalPost
+          key={id}
+          id={id}
+          title={title}
+          body={body}
+          author={author}
+          created={created}
+          media={media}
+        />
+        <Modal.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            {createError && <FormError>{createError}</FormError>}
+            {updated && (
+              <Alert variant="success">Your comment was successful.</Alert>
+            )}
+            <fieldset disabled={submitting}>
+              <Form.Group className="mb-3" controlId="commentBody">
+                <Form.Control
+                  as="textarea"
+                  placeholder="Comment on post"
+                  aria-describedby="commentBody"
+                  {...register("body")}
+                />
+                {errors.body && (
+                  <FormWarning>{errors.body.message}</FormWarning>
+                )}
+              </Form.Group>
+              <div className="form-create-button">
+                <Button variant="primary" type="submit">
+                  {submitting ? "Commenting..." : "Comment"}
+                </Button>
+              </div>
+            </fieldset>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
