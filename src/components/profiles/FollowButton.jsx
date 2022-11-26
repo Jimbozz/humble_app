b6878e2ee6d2245f539c9b4055ec44dd4a81ee3d
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-26 14:57:03 +0100

    initial

---
+++ src/components/profiles/FollowButton.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import useAxios from "../../hooks/useAxios";
+
+export default function FollowButton({ name, getProfile }) {
+  const http = useAxios();
+  const followUrl = "social/profiles/" + name + "/follow";
+  const unFollowUrl = "social/profiles/" + name + "/unfollow";
+
+  const [following, setFollowing] = useState(false);
+
+  const followUser = async () => {
+    try {
+      const response = await http.put(followUrl);
+      console.log("response", response.data);
+      setFollowing(true);
+      getProfile();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const unFollowUser = async () => {
+    try {
+      const response = await http.put(unFollowUrl);
+      console.log("response", response.data);
+      setFollowing(false);
+      getProfile();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   unFollowUser();
+  // });
+
+  return (
+    <>
+      {/* {following ? (
+        <Button onClick={unFollowUser} variant="outline-danger">
+          Un-follow
+        </Button>
+      ) : (
+        <Button onClick={followUser} variant="primary">
+          Follow
+        </Button>
+      )} */}
+      <Button onClick={unFollowUser} variant="outline-danger">
+        Un-follow
+      </Button>
+
+      <Button onClick={followUser} variant="primary">
+        Follow
+      </Button>
+    </>
+  );
+}
