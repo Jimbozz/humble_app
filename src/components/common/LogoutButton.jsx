3e00994cd391385041e314e7e89036215e1d08d3
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-27 16:51:11 +0100

    initial

---
+++ src/components/common/LogoutButton.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Dropdown } from "react-bootstrap";
+import { BiLogOut } from "react-icons/bi";
+import { NavLink, useNavigate } from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
+
+export default function LogoutButton({ activeStyle }) {
+  const [, setAuth] = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  function logoutUser() {
+    setAuth(null);
+    navigate("/login");
+  }
+  return (
+    <NavLink
+      onClick={logoutUser}
+      style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      to="/login"
+      className="sidebar-content__list--item sidebar-content__list--item-none nav-link">
+      <BiLogOut />
+      <span className="sidebar-content__list--name">Logout</span>
+    </NavLink>
+  );
+}
