2e718c260917a2725635dc195ec1f938f38eac13
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-29 16:32:47 +0100

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import { Routes, Route, Router, Switch, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./sass/styles.scss";
 import HomePage from "./components/home/HomePage";
-import Layout from "./layout/Layout";
 import ProfilesPage from "./components/profiles/ProfilesPage";
 import { AuthProvider } from "./context/AuthContext";
 import RegisterPage from "./components/register/RegisterPage";
 import LoginPage from "./components/login/LoginPage";
-
 import ProfileSpecificPage from "./components/profile-specific/ProfileSpecificPage";
 import PostSpecificPage from "./components/post-specific/PostSpecificPage";
+import AdminProfile from "./components/profile-specific/AdminProfile";
+import { ProtectedRoute } from "./components/common/ProtectedRoute";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
         <Route exact path="/register" element={<RegisterPage />} />
         <Route exact path="/login" element={<LoginPage />} />
+        <Route
+          exact
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="post">
-          <Route path=":id/:title" element={<PostSpecificPage />} />
+          <Route
+            path=":id/:title"
+            element={
+              <ProtectedRoute>
+                <PostSpecificPage />
+              </ProtectedRoute>
+            }
+          />
         </Route>
-
-        <Route path="/profiles" element={<ProfilesPage />} />
-        <Route path="/profiles/:id" element={<ProfileSpecificPage />} />
-        <Route path="/profiles/:name" element={<ProfileSpecificPage />} />
-        {/* <Route path="profiles">
-          <Route path=":id" element={<ProfileSpecificPage />} />
-          <Route path=":id/:id" element={<ProfileSpecificPage />} />
-          <Route path=":name" element={<ProfileSpecificPage />} />
-        </Route> */}
+        <Route
+          exact
+          path="/my-profile/:id"
+          element={
+            <ProtectedRoute>
+              <AdminProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          exact
+          path="/profiles"
+          element={
+            <ProtectedRoute>
+              <ProfilesPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          exact
+          path="/profiles/:id"
+          element={
+            <ProtectedRoute>
+              <ProfileSpecificPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </AuthProvider>
   );
