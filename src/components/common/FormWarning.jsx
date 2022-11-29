979eb5fda1b1b375642ecb6d71dc55f779cad83d
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-29 16:33:38 +0100

    removed unused import

--- src/components/common/FormWarning.jsx
+++ src/components/common/FormWarning.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import Alert from "react-bootstrap/Alert";
 
 export default function FormWarning({ children }) {
   return <small className="form-warning">{children}</small>;
