71f6802b0e62273bafc752142f1b7d27b8209e0e
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-29 10:39:49 +0100

    added helmet title

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,10 +1,14 @@
 import Heading from "../common/Heading";
 import Layout from "../../layout/Layout";
 import PostsList from "../posts/PostsList";
+import { Helmet } from "react-helmet-async";
 
 function HomePage() {
   return (
     <Layout>
+      <Helmet>
+        <title>Humble | Home</title>
+      </Helmet>
       <Heading content="Home Page" />
       <PostsList />
     </Layout>
--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -2,6 +2,7 @@ import { Button, Container } from "react-bootstrap";
 import LoginForm from "./LoginForm";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -11,23 +12,30 @@ export default function LoginPage() {
   }
 
   return (
-    <Container className="form-wrapper d-flex flex-column align-items-center justify-content-center">
-      <div className="d-flex flex-column align-items-center">
-        <div className="form-logo">
-          <img
-            className="form-logo__image"
-            src={require("../../assets/humble-logo.png")}
-            alt="humble logo"
-          />
+    <>
+      <Helmet>
+        <title>Humble | Login</title>
+      </Helmet>
+      <Container className="form-wrapper d-flex flex-column align-items-center justify-content-center">
+        <div className="d-flex flex-column align-items-center">
+          <div className="form-logo">
+            <img
+              className="form-logo__image"
+              src={require("../../assets/humble-logo.png")}
+              alt="humble logo"
+            />
+          </div>
+          <LoginForm />
+          <div>
+            <small>Need to register a new account?</small>
+            <Button
+              variant="link"
+              onClick={() => handleButtonClick("/register")}>
+              Register here
+            </Button>
+          </div>
         </div>
-        <LoginForm />
-        <div>
-          <small>Need to register a new account?</small>
-          <Button variant="link" onClick={() => handleButtonClick("/register")}>
-            Register here
-          </Button>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   );
 }
