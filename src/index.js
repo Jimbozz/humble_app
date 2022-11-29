cf7c5e91818190dc1b80ea5133d54c120980fde6
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-29 10:39:31 +0100

    added helmet provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./App.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
